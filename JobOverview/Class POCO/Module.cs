c3c6c7d1fa8197a319c19c6ca2f50a0bfed7063b
bguillaumin <bguillaumin@BGUILLAUMI17-DE>
2017-05-04 17:49:06 +0200

    maj

---
+++ JobOverview/Class POCO/Module.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOverview
+{
+    public class Module : IEquatable<Module>
+    {
+        public string CodeModule { get; set; }
+        public string Libellé { get; set; }
+
+        bool IEquatable<Module>.Equals(Module other)
+        {
+            if (other == null) return false;
+            if (CodeModule == other.CodeModule) return true;
+            return false;
+        }
+    }
+}
---
+++ JobOverview/Class POCO/Version.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOverview
+{
+    public class Version : IEquatable<Version>
+    {
+        public float NumeroVersion { get; set; }
+        public short Millesime { get; set; }
+        public DateTime DateOuverture { get; set; }
+        public DateTime DateSortiePrevue { get; set; }
+        public short LastNumeroRelease { get; set; }
+
+        bool IEquatable<Version>.Equals(Version other)
+        {
+            if (other == null) return false;
+            if (NumeroVersion == other.NumeroVersion) return true;
+            return false;
+        }
+    }
+}
